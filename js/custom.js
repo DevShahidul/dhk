b0bf6b4af3bd729918aa5cfc20edca35c731eeaa
Shahidul <ishahin56@gmail.com>
2019-03-05 21:09:54 +0600

    Added animation and complete project single page

--- js/custom.js
+++ js/custom.js
@@ -7,5 +7,9 @@
         $('.dropdown').on('hide.bs.dropdown', function(e){
             $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
         });
+    }) // End ready function
+    
+    $(window).on("load", function(){
+        $("#sidebar-container .logo, .main-header .navbar-brand").addClass("active");
     })
 })(jQuery)
